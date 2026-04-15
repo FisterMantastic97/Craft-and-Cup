1d8131556452fe60c0a27a926ee596a701bb7f77
Nick <NicholasSMitchell@gmail.com>
2026-04-15 00:56:19 -0700

    Add PWA manifest and Apple meta tags

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,9 +1,21 @@
 import { Html, Head, Main, NextScript } from "next/document";
-
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0e0e0e" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="Craft & Cup" />
+        <link rel="apple-touch-icon" href="/icons/apple-touch-icon.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/icons/icon-76.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/icons/icon-120.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/icons/icon-152.png" />
+        <link rel="apple-touch-icon" sizes="167x167" href="/icons/icon-167.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/icon-180.png" />
+        <link rel="icon" type="image/png" href="/icons/favicon.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
