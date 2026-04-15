134b3ccffcb43e35a97720eded42db57dbdb4e4d
Nick <NicholasSMitchell@gmail.com>
2026-04-15 02:41:11 -0700

    Fix deprecated web app meta tag

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,6 +6,7 @@ export default function Document() {
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, viewport-fit=cover" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#0e0e0e" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="Craft & Cup" />
