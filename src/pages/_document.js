72a0f4a147c5031acacc8d1ae0c28435e602a341
Nick <NicholasSMitchell@gmail.com>
2026-04-15 01:26:22 -0700

    Block pinch zoom except on flavor wheels

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,6 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
         <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#0e0e0e" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
