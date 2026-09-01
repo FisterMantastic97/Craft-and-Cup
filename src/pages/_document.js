1c0349799b3f72a48535d32ad875f433684e67f9
FisterMantastic97 <nicholassmitchell@gmail.com>
2026-08-31 20:27:46 -0700

    Fonts: one link covering every weight and italic used

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -36,7 +36,7 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@300;400;600&family=Jost:wght@300;400;500&display=swap"
+          href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,600&family=Jost:wght@300;400;500;600;700&display=swap"
           rel="stylesheet"
         />
       </Head>
